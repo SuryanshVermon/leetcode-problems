802d501f8ed3e343f1e8910c80d02b3764bea4b5
suryansh vermon <114719728+SuryanshVermon@users.noreply.github.com>
2024-09-03 17:55:32 +0530

    Time: 0 ms (100%), Space: 7.9 MB (77.66%) - LeetHub

--- 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
+    int sumDigits(int num) {
+        int sum = 0;
+        while(num) {
+            sum += num%10;
+            num /= 10;
+        }
+        return sum;
+    }
+    
     int getLucky(string s, int k) {
-        string number ="";
-        for(char x :s){
-            number +=to_string(x-'a'+1);
-        }
+        int sum = 0;
+        for(char &ch : s) {
+		    int val = ch-'a'+1;
+		    sum += val < 10 ? val : (val%10 + val/10);
+		}
 
-        while(k>0){
-            int temp=0;
-            for(char x :number){
-                temp +=x -'0';
-            }
-            number = to_string(temp);
-            k--;
-        }
-        return stoi(number);
+        k -= 1;
+        while(k-- && sum >= 10) 
+            sum = sumDigits(sum);
+        
+        return sum;
     }
 };
