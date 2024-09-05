b85e3d3c40bd265377fb0bd33ee5a68fb99fc2ef
suryansh vermon <114719728+SuryanshVermon@users.noreply.github.com>
2024-09-05 13:21:45 +0530

    Time: 12 ms (14.01%), Space: 11.2 MB (8.32%) - LeetHub

--- 1186-building-h2o/1186-building-h2o.cpp
+++ 1186-building-h2o/1186-building-h2o.cpp
@@ -1,30 +1,34 @@
-class H2O {
-    int d;
-    std::mutex mtx;
-    std::condition_variable cv;
-    
+class H2O 
+{
+
+private:
+    mutex mtx;
+    condition_variable cv;
+    size_t count;   
+
 public:
-    H2O(): d(0) {
+    H2O():count(0)
+    {
         
     }
 
-    void hydrogen(function<void()> releaseHydrogen) {
-        unique_lock<std::mutex> lck(mtx);
-        while (d >= 2) {
-            cv.wait(lck);
-        }
-        releaseHydrogen();
-        d++;
-        cv.notify_all();
-    }
+    void hydrogen(function<void()> releaseHydrogen) 
+    {
+        
+        unique_lock<mutex> lock(mtx);
 
-    void oxygen(function<void()> releaseOxygen) {
-        unique_lock<std::mutex> lck(mtx);
-        while (d < 0) {
-            cv.wait(lck);
+        cv.wait(lock,[this](){return count != 2 ;} );
+        count++;
+        releaseHydrogen();
+        cv.notify_all();
         }
+
+    void oxygen(function<void()> releaseOxygen) 
+    {
+        std::unique_lock<mutex> lock(mtx);
+        cv.wait(lock, [this](){return count == 2;});
         releaseOxygen();
-        d-=2;
+        count = 0;
         cv.notify_all();
     }
 };
