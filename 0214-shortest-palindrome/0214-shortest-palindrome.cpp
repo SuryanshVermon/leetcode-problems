de5f5fc03cf636aa2952d92d06796e3039177a0c
suryansh vermon <114719728+SuryanshVermon@users.noreply.github.com>
2024-09-21 00:48:02 +0530

    Time: 5 ms (74.26%), Space: 11.8 MB (32.64%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -1,16 +1,41 @@
 class Solution {
 public:
+    
+    void computeLPS(string pattern, vector<int>& lps) {
+        int M = pattern.length();
+        int len = 0; 
+    
+        lps[0] = 0; 
+    
+        int i = 1;
+        while (i < M) {
+            if (pattern[i] == pattern[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1]; 
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
     string shortestPalindrome(string s) {
         string rev = s;
         reverse(begin(rev), end(rev));
+           string temp = s + "-" + rev;
 
-        for(int i = 0; i < s.length(); i++) {
+        vector<int> LPS(temp.length(), 0); 
+        computeLPS(temp, LPS);
 
-            if(!memcmp(s.c_str(), rev.c_str() + i, s.length()-i)) { 
-                return rev.substr(0, i) + s;
-            }
-        }
+        int longestLPSLength = LPS[temp.length()-1]; 
 
-        return rev + s;
+        string culprit = rev.substr(0, s.length() - longestLPSLength); 
+
+        return culprit + s;
     }
 };
+
