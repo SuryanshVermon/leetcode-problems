514afa266edffb408003ba9bfaddb340c00c8077
suryansh vermon <114719728+SuryanshVermon@users.noreply.github.com>
2024-10-09 18:52:05 +0530

    Time: 4 ms (30.06%), Space: 7.8 MB (62.93%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     int maxScore(string s) {
-        int n= s.length();
-        int result =INT_MIN;
-        for(int i=0;i<=n-2;i++){
-           int zero_count =0;
+        int n=s.length();
+        int score = INT_MIN;
 
-           for(int j=0;j<=i;j++){
-            if(s[j]=='0'){
-                zero_count ++;
-            }
-           }
+        int zeros = 0;
+        int ones =0;
 
-           int one_count =0;
-           for(int j=i+1;j<n;j++){
-            if(s[j]=='1'){
-                one_count ++;
-            }
-           }
-           result =max(result,one_count+zero_count);
+        for(int i=0;i<n-1;i++){
+            if(s[i]=='1') ones++;
+            else zeros ++;
+            
+            score = max(score,zeros - ones);
         }
-        return result;
+        if(s[n-1]=='1'){
+            ones ++;
+        }
+        return score+ones;        
     }
 };
