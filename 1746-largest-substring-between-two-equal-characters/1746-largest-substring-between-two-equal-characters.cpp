b3bc2112e3f36fba972c79230dfd40329220f6ca
suryansh vermon <114719728+SuryanshVermon@users.noreply.github.com>
2024-10-11 01:10:50 +0530

    Time: 0 ms (100%), Space: 7.7 MB (82.12%) - LeetHub

---
+++ 1746-largest-substring-between-two-equal-characters/1746-largest-substring-between-two-equal-characters.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+       int n =s.length();
+        int result = -1;;
+       for(int i =0;i<n;i++){
+        for(int j=i;j<n;j++){
+            if(s[i] ==s[j]){
+                result = max(result,j-i-1);
+            }
+        }
+       } 
+       return result;
+    }
+};
