c8c422c76403374d87b0068f22927ac5d38b4e1d
suryansh vermon <114719728+SuryanshVermon@users.noreply.github.com>
2024-10-10 18:04:56 +0530

    Time: 35 ms (72.68%), Space: 47 MB (37.47%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
-
     int maxWidthRamp(vector<int>& nums) {
-
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         int n = nums.size();
         stack<int> st;
-        for(int i = 0; i < n; i++){
-            if(st.empty() || nums[st.top()] > nums[i]){
+        
+        st.push(0);
+
+        for(int i = 1; i < n; i++){
+            if(nums[st.top()] > nums[i]){
                 st.push(i);
             }
         }
+
         int ans = 0;
-        for(int i = n-1; i > 0; i--){
-            while(!st.empty() && nums[st.top()] <= nums[i]){
+
+        for(int i = n-1; i >= 0; ){
+
+            while(!st.empty() and nums[st.top()] <= nums[i]){
                 ans = max(ans, i - st.top());
                 st.pop();
             }
+
+            if(st.empty()) break;
+
+            i--;
         }
+
         return ans;
+
     }
 };
