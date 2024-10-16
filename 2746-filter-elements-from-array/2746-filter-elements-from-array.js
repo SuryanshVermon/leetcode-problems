d4edfcd6223d6240517c43515d728e930e125851
suryansh vermon <114719728+SuryanshVermon@users.noreply.github.com>
2024-10-16 16:05:20 +0530

    Time: 60 ms (18.09%), Space: 49.2 MB (20.73%) - LeetHub

--- 2746-filter-elements-from-array/2746-filter-elements-from-array.js
+++ 2746-filter-elements-from-array/2746-filter-elements-from-array.js
@@ -3,12 +3,12 @@
  * @param {Function} fn
  * @return {number[]}
  */
-var filter = function (arr, fn) {
-    var filteredArr = []; // Removed void
-    for (var i = 0; i < arr.length; i++) { // Fixed the for loop parentheses
+var filter = function(arr, fn) {
+    var filteredArr = [];
+    for (var i = 0; i < arr.length; i++) {
         if (fn(arr[i], i)) {
             filteredArr.push(arr[i]);
         }
     }
-    return filteredArr; // Added semicolon
+    return filteredArr;
 };
