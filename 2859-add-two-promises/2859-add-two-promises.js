89bf29f850564f4332e2e97f2d31e3788c3cf233
suryansh vermon <114719728+SuryanshVermon@users.noreply.github.com>
2024-10-17 10:19:41 +0530

    Time: 60 ms (71.22%), Space: 49.1 MB (91.52%) - LeetHub

--- 2859-add-two-promises/2859-add-two-promises.js
+++ 2859-add-two-promises/2859-add-two-promises.js
@@ -3,10 +3,9 @@
  * @param {Promise} promise2
  * @return {Promise}
  */
-var addTwoPromises = async function(promise1, promise2) {
-    const[value1,value2] = await Promise.all([promise1,promise2]);
-
-    return value1 + value2;
+var addTwoPromises = async (promise1, promise2) => {
+   const [value1, value2] = await Promise.all([promise1, promise2])
+   return value1 + value2 
 };
 
 /**
