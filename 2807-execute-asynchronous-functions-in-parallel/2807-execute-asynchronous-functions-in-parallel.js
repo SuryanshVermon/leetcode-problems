125652ab16e3d2e59fdab8cbed4eeb81d31f6338
suryansh vermon <114719728+SuryanshVermon@users.noreply.github.com>
2024-10-17 10:31:12 +0530

    Time: 74 ms (45.55%), Space: 49.9 MB (39.4%) - LeetHub

--- 2807-execute-asynchronous-functions-in-parallel/2807-execute-asynchronous-functions-in-parallel.js
+++ 2807-execute-asynchronous-functions-in-parallel/2807-execute-asynchronous-functions-in-parallel.js
@@ -1,18 +1,25 @@
-var promiseAll = async function(functions) {
-    return await new Promise((resolve, reject) => {
-
-        let arr = Array(functions.length);
-        let waitingFor = functions.length;
-
-        for(let i = 0; i < functions.length; ++i){
-            functions[i]()
-                .then((result) => { 
-                    arr[i] = result; 
-                    if (--waitingFor === 0) resolve(arr);
-                }).catch(reject);
-        }
-    });
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any>}
+ */
+var promiseAll = function(functions) {
+    let cnt = 0;
+    const ans = [];
+    return new Promise((resolve, reject) => {
+        functions.forEach((fn, index) => {
+            fn()
+            .then(res => {
+                cnt += 1
+                ans[index] = res
+                if (cnt == functions.length) resolve(ans);
+            })
+            .catch(err => {
+                reject(err)
+            })
+        })
+    })
 };
+
 /**
  * const promise = promiseAll([() => new Promise(res => res(42))])
  * promise.then(console.log); // [42]
