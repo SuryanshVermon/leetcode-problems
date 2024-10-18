213fa97988002a04482d43eb88de0c3ac498ac16
suryansh vermon <114719728+SuryanshVermon@users.noreply.github.com>
2024-10-18 23:40:48 +0530

    Time: 2 ms (99.88%), Space: 12.6 MB (24.11%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,36 +1,15 @@
 class Solution {
 public:
-    //O(2^n)
-    int countSubsets(int idx, int currOr, vector<int>& nums, int maxOr, vector<vector<int>>& t) {
-        if(idx == nums.size()) {
-            if(currOr == maxOr)
-                return t[idx][currOr] = 1; //Found one subset
-            return t[idx][currOr] = 0;
+    int countMaxOrSubsets(vector<int>& nums) {
+        ios::sync_with_stdio(0); cin.tie(0);
+        map<int, int, greater<int>> mp;
+        
+        for(int num : nums) {
+            for(const auto& p : mp) {
+                mp[p.first|num] += p.second;
+            }
+            mp[num]++;
         }
-
-        if(t[idx][currOr] != -1) {
-            return t[idx][currOr];
-        }
-
-        //Take nums[idx]
-        int takeCount = countSubsets(idx+1, currOr | nums[idx], nums, maxOr, t);
-
-        //Not taken nums[idx]
-        int notTakeCount = countSubsets(idx+1, currOr, nums, maxOr, t);
-
-
-        return t[idx][currOr] = takeCount + notTakeCount;
-
-    }
- int countMaxOrSubsets(vector<int>& nums) {
-        int maxOr = 0;
-        for(int &num : nums) {
-            maxOr |= num;
-        }
-        int n = nums.size();
-
-        vector<vector<int>> t(n+1, vector<int>(maxOr+1, -1));
-        int currOr = 0;
-        return countSubsets(0, currOr, nums, maxOr, t);
+        return mp.begin()->second;
     }
 };
