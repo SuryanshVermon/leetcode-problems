ccb320b010bdb85441a1ab86f0c7e22fdc8769a4
suryansh vermon <114719728+SuryanshVermon@users.noreply.github.com>
2024-10-19 01:24:26 +0530

    Time: 7 ms (100%), Space: 10.6 MB (77.96%) - LeetHub

--- 1534-minimum-number-of-frogs-croaking/1534-minimum-number-of-frogs-croaking.cpp
+++ 1534-minimum-number-of-frogs-croaking/1534-minimum-number-of-frogs-croaking.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     int minNumberOfFrogs(string croakOfFrogs) {
-        int c=0,r=0,o=0,a=0,k=0;
-        int ans=INT_MIN;
-        int frogs=0;
-        int n=croakOfFrogs.size();
-        for(int i=0;i<n;i++){
-            switch(croakOfFrogs[i]){
-                case 'c':
-                    frogs++;
-                    c++;
-                    break;
-                case 'r':
-                    r++;
-                    break;
-                case 'o':
-                    o++;
-                    break;
-                case 'a':
-                    a++;
-                    break;
-                case 'k':
-                    k++;
-                    frogs--;
-                    break;
+        int count[5] = {0};
+        int frogs = 0, maxFrogs = 0;
+        string croak = "croak";
+
+        for (char ch : croakOfFrogs) {
+            int index = croak.find(ch);
+            if (index == string::npos)
+                return -1;
+
+            count[index]++;
+            if (index > 0 && count[index] > count[index - 1])
+                return -1;
+
+            if (index == 4) {
+                frogs--;
+                for (int i = 0; i < 5; i++)
+                    count[i]--;
+            } else if (index == 0) {
+                frogs++;
+                maxFrogs = max(maxFrogs, frogs);
             }
-            ans=max(ans,frogs);
-            if(c<r || r<o || o<a || a<k) return -1;
-            
         }
-        return frogs==0?ans:-1;
+
+        return frogs == 0 ? maxFrogs : -1;
     }
 };
