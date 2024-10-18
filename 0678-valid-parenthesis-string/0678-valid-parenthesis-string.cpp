ccab791a82d1b702e4ad9f038736de1bbac8d85b
suryansh vermon <114719728+SuryanshVermon@users.noreply.github.com>
2024-10-19 00:47:33 +0530

    Time: 0 ms (100%), Space: 7.5 MB (85.89%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,29 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int low = 0, high = 0;
+
+        for (char c : s) {
+            if (c == '(') {
+                low++;
+                high++;
+            } else if (c == ')') {
+                low = max(low - 1, 0);
+                high--;
+            } else { 
+                low = max(low - 1, 0);
+                high++;
+            }
+
+            if (high < 0) {
+                return false;
+            }
+        }
+
+        return low == 0;
+    }
+};
